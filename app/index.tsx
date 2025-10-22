47890350749ff58b33c489ca3772c85c0d72f257
aganepesAI <aaganepes@gmail.com>
2025-10-22 11:31:13 +0500

    first-projects

---
+++ app/index.tsx
@@ -0,0 +1,93 @@
+import { View,Text, StyleSheet,Image,TouchableOpacity,Dimensions,type ColorValue } from "react-native";
+import {Link} from 'expo-router';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
+const pathImage = require("../assets/images/emoji6.png");
+const urlImage = "https://codebrahma.com/static/8c27233fcb0ff4aa1bf135f164c98dbd/41099/brickwall.jpg";
+
+
+
+type ButtonPromp={
+    bgColor?:ColorValue,
+    title:string,
+    onPress:(() => void) | undefined
+}
+
+function MyCustomButton({bgColor,title,onPress}:ButtonPromp){
+    return (
+        <TouchableOpacity style={[styles.couterButton,{backgroundColor:(!bgColor ? "lightblue": bgColor)}]} onPress={onPress}>
+            <Text style={{color:"black"}}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default function HomeScreen(){
+    const [couter,setCouter]=useState<number>(0);
+    return (
+        <View style={styles.Container}>
+            <Image source={{uri:urlImage}} style={styles.image} />
+            <View style={styles.contentContainer}>
+                <Text style={{fontSize:24,marginTop:16}}>Couter: {couter}</Text>
+                <Text style={styles.subtitle}>Click buttons to change the counter.</Text>
+                <View style={styles.buttonConatiner}>
+                    <MyCustomButton title={"Increment +"} onPress={()=>setCouter(couter+1)}/>
+                    <MyCustomButton title={"Decrement -"} bgColor={"pink"} onPress={()=>setCouter(couter-1)}/>
+                 </View>
+            </View>
+            
+            <Link href={"/todo"} style={styles.button}>
+                <Text>Go To Do list page</Text>
+            </Link>
+        </View>
+    )
+}
+
+
+
+
+const styles= StyleSheet.create({
+    Container:{
+        boxSizing:"border-box",
+        flex:1,
+        backgroundColor:"red",
+        alignItems:"center",
+        paddingVertical:16
+    },
+    contentContainer:{
+        alignSelf:"flex-start",
+        paddingHorizontal:16
+    },
+    subtitle:{
+        color:"darkgray",
+        fontSize:18,
+    },
+    image:{
+        boxSizing:"border-box",
+        width:"95%",
+        height:"30%",
+        borderRadius:5
+    },
+    button:{
+        fontSize:16,
+        borderRadius:5,
+        backgroundColor:"black",
+        padding:5,
+        color:"white",
+        width:150,
+        alignItems:"center"
+    },
+    couterButton:{
+        flex:.5,
+        fontSize:16,
+        borderRadius:5,
+        alignItems:"center",
+        padding:5,
+    },
+    buttonConatiner:{
+        flex:1,
+        flexDirection:"row",
+        gap:10,
+        marginVertical:16
+    }
+})
---
+++ app/todo.tsx
@@ -0,0 +1,128 @@
+import { View,Text,StyleSheet,TextInput,TouchableOpacity,FlatList } from "react-native";
+import { Link } from 'expo-router';
+import { useEffect, useState } from "react";
+
+export default function AboutScreen(){
+    const [inputText,setInput] = useState("");
+    const [itemArray,setItemArray] = useState<item[]>([]);
+
+    const AddItem=(content:string)=>{
+        const item:item ={id:itemArray.length+1 , content}
+        if(content) setItemArray(items=>[...items,item]);
+        setInput("")
+    }
+    const RemoveItem=(id:number)=>{
+        setItemArray(items=>items.filter(v=>v.id!==id))
+    }
+    type item={
+        id:number,
+        content:string
+    }
+    interface ItemPromp{
+        item:item,
+        RemoveItem:(id:number)=>void
+    }
+    // :React.FC<ItemPromp>
+    const ItemComponent=({item,RemoveItem}:ItemPromp)=>(
+            <View style={[styles.item,
+            {borderBottomWidth:1,borderColor: '#e6bbbbff',paddingBottom:3}]}>
+                <Text>{item.id}: {item.content}</Text>
+                <TouchableOpacity 
+                    onPress={()=>RemoveItem(item.id)}
+                    style={styles.deleteButton}
+                    >
+                    <Text style={{color:"red"}}>X</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    useEffect(()=>{
+        console.log(itemArray)
+    },[itemArray])
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    onChangeText={(newText)=>setInput(newText)} 
+                    value={inputText}
+                    placeholder="Input giriz .."
+                    placeholderTextColor={"gray"}
+                    style={styles.inputElement} 
+                    />
+                <TouchableOpacity style={styles.addButton} onPress={()=>{AddItem(inputText)}}>
+                    <Text>Add</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.itemContainer}>
+                {/* <FlatList
+                    data={itemArray}
+                    renderItem={({item})=><ItemComponent item={item} RemoveItem={RemoveItem}/>}
+                    keyExtractor={(item)=>item.id.toString()}
+                /> */}
+                {itemArray.length ? 
+                itemArray.map( (v) =>(<ItemComponent item={v} RemoveItem={RemoveItem} key={v.id}/>)) 
+                : ""}
+            </View>
+            <Link href={"/"} style={styles.linkButton}>
+                <Text>Go Couter page</Text>
+            </Link>
+        </View>
+    )
+}
+
+
+
+const styles=StyleSheet.create({
+    container:{
+        padding:10,
+        flex:1,
+        alignItems:"center",
+        gap:20,
+    },
+    inputContainer:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        marginBottom:16,
+        width:300,
+    },
+    inputElement:{
+        borderWidth:1,
+        borderRadius:5,
+        padding:5,
+        marginRight:16,
+        flex:3
+    },
+    addButton:{
+        backgroundColor:"gray",
+        padding:5,
+        borderRadius:5,
+        color:"red",
+        textTransform:"uppercase",
+        fontWeight:"600"
+    },
+    itemContainer:{
+        gap:10,
+        alignItems:"flex-start",
+        width:300,
+        paddingHorizontal:10
+    },
+    item:{
+        flexDirection:'row',
+        width:"100%",
+        justifyContent:"space-between",
+        alignItems:"center",
+        gap:10
+    },
+    deleteButton:{
+        backgroundColor:"gray",
+        padding:10,
+        paddingVertical:5,
+        borderRadius:5,
+    },
+    linkButton:{
+        backgroundColor:"black",
+        borderRadius:5,
+        color:'white',
+        padding:5
+    }
+})
