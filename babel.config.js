ec57af78f9c225b4293c38b1684e32dda3efbb88
Aganepes <aaganepes@gmail.com>
2025-10-23 12:21:00 +0500

    add babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-module.exports = function(api) {
+// babel.config.js
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [require.resolve('expo-router/babel')],
+    plugins: [
+      require.resolve('expo-router/babel'),
+    ],
   };
 };
